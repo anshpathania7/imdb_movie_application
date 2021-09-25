e8a3556e3f42154b70423d5699ffb1f2a96baf29
anshpathania7 <anshupathania7@gmail.com>
2021-09-25 17:05:57 +0530

    Fix : rendering issues

--- lib/screen/imdb_screen/imdb_movie_screen.dart
+++ lib/screen/imdb_screen/imdb_movie_screen.dart
@@ -37,128 +37,141 @@ class _ImdbMovieScreenState extends State<ImdbMovieScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kDarkBlue,
-      body: ChangeNotifierProvider(
-        create: (context) => ImdbMovieProvider(),
-        builder: (context, child) => Consumer<ImdbMovieProvider>(
-          builder: (context, state, child) => Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Column(
             children: [
-              SizedBox(height: 60),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: TextFormField(
-                  controller: _editingController,
-                  style: TextStyle(color: Colors.white70),
-                  onFieldSubmitted: (val) {
-                    state.getDataForThisMovie(movieTitle: val);
-                  },
-                  decoration: InputDecoration(
-                    contentPadding:
-                        const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
-                    hintText: "Search For Movies",
-                    hintStyle: TextStyle(color: Colors.grey),
-                    fillColor: Colors.grey.withOpacity(0.3),
-                    filled: true,
-                    prefixIcon: IconButton(
-                      onPressed: () {
-                        state.getDataForThisMovie(
-                            movieTitle: _editingController.text);
-                        FocusManager.instance.primaryFocus?.unfocus();
-                      },
-                      icon: Image(
-                        image: Pngs.search,
-                        color: Colors.grey,
-                        height: 22,
-                        width: 22,
-                      ),
-                    ),
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        _editingController.clear();
-                        FocusManager.instance.primaryFocus?.unfocus();
-                      },
-                      icon: Image(
-                        image: Pngs.cross,
-                        color: Colors.grey,
-                        height: 22,
-                        width: 22,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(color: kDarkBlue)),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(color: kDarkBlue)),
-                  ),
-                ),
+              SizedBox(height: 100),
+              Image(
+                image: Pngs.bgBloop,
               ),
-              if (state.isLoadingData) ...[
-                Center(child: CircularProgressIndicator())
-              ],
-              if (state.imdbData.isEmpty &&
-                  !state.isLoadingData &&
-                  !state.isIntial &&
-                  !state.hasSuccessfullyLoadedData) ...[
-                Center(
-                  child: Text(
-                    "Try again with different movie name",
-                    style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
+            ],
+          ),
+          ChangeNotifierProvider(
+            create: (context) => ImdbMovieProvider(),
+            builder: (context, child) => Consumer<ImdbMovieProvider>(
+              builder: (context, state, child) => Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 60),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: TextFormField(
+                      controller: _editingController,
+                      style: TextStyle(color: Colors.white70),
+                      onFieldSubmitted: (val) {
+                        state.getDataForThisMovie(movieTitle: val);
+                      },
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 4, horizontal: 12),
+                        hintText: "Search For Movies",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        fillColor: Colors.grey.withOpacity(0.3),
+                        filled: true,
+                        prefixIcon: IconButton(
+                          onPressed: () {
+                            state.getDataForThisMovie(
+                                movieTitle: _editingController.text);
+                            FocusManager.instance.primaryFocus?.unfocus();
+                          },
+                          icon: Image(
+                            image: Pngs.search,
+                            color: Colors.grey,
+                            height: 22,
+                            width: 22,
+                          ),
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            _editingController.clear();
+                            FocusManager.instance.primaryFocus?.unfocus();
+                          },
+                          icon: Image(
+                            image: Pngs.cross,
+                            color: Colors.grey,
+                            height: 22,
+                            width: 22,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(color: kDarkBlue)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(color: kDarkBlue)),
+                      ),
                     ),
                   ),
-                ),
-              ],
-              if (state.imdbData.isNotEmpty) ...[
-                SizedBox(height: 40),
-                Expanded(
-                  child: SmartRefresher(
-                    controller: _controller,
-                    physics: BouncingScrollPhysics(),
-                    enablePullUp: true,
-                    enablePullDown: false,
-                    onLoading: () async {
-                      _controller..requestLoading();
-                      await state.loadMoreData();
-                      _controller.loadComplete();
-                    },
-                    child: ListView.builder(
-                      itemCount: state.imdbData.length,
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, i) => Padding(
-                        padding: const EdgeInsets.only(bottom: 12.0),
-                        child: InkWell(
-                          onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              maintainState: true,
-                              builder: (context) => ImdbDetailedScreen(
-                                data: state.imdbData[i],
+                  if (state.isLoadingData) ...[
+                    Center(child: CircularProgressIndicator())
+                  ],
+                  if (state.imdbData.isEmpty &&
+                      !state.isLoadingData &&
+                      !state.isIntial &&
+                      !state.hasSuccessfullyLoadedData) ...[
+                    Center(
+                      child: Text(
+                        "Try again with different movie name",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ],
+                  if (state.imdbData.isNotEmpty) ...[
+                    SizedBox(height: 40),
+                    Expanded(
+                      child: SmartRefresher(
+                        controller: _controller,
+                        physics: BouncingScrollPhysics(),
+                        enablePullUp: true,
+                        enablePullDown: false,
+                        onLoading: () async {
+                          _controller..requestLoading();
+                          await state.loadMoreData();
+                          _controller.loadComplete();
+                        },
+                        child: ListView.builder(
+                          itemCount: state.imdbData.length,
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, i) => Padding(
+                            padding: const EdgeInsets.only(bottom: 12.0),
+                            child: InkWell(
+                              onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  maintainState: true,
+                                  builder: (context) => ImdbDetailedScreen(
+                                    data: state.imdbData[i],
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(bottom: 20.0),
-                            child: ImdbCard(
-                              indexOfData: i,
-                              duration: state.imdbData[i].runtime,
-                              title: state.imdbData[i].title,
-                              poster: state.imdbData[i].poster,
-                              imdbRating: state.imdbData[i].imdbRating,
+                              child: Padding(
+                                padding: const EdgeInsets.only(bottom: 20.0),
+                                child: ImdbCard(
+                                  indexOfData: i,
+                                  duration: state.imdbData[i].runtime,
+                                  title: state.imdbData[i].title,
+                                  poster: state.imdbData[i].poster,
+                                  imdbRating: state.imdbData[i].imdbRating,
+                                ),
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-              ]
-            ],
+                  ]
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
